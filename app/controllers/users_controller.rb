16b0e84a8b10e6b511974a6979ae70dd69de79cd
miyukimaura <maumiyu0412@gmail.com>
2019-06-23 13:38:33 +0900

    users_controller　indexアクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
 
@@ -10,7 +19,16 @@ class UsersController < ApplicationController
     end
   end
 
+  private #←いるかいらないか分からない
+
   def user_params
     params.require(:user).permit(:name,:email)
   end
 end
+
+
+
+
+
+
+
