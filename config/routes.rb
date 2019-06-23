2e336fd270d5dda61510805a76b8176c01ffa40d
miyukimaura <maumiyu0412@gmail.com>
2019-06-23 13:23:37 +0900

    ルーティングに　users#index　追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,19 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'groups#index'
+
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
+
+  resources :users, only: :show do
+    collection do
+      get 'search'
+    end
+  end
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
