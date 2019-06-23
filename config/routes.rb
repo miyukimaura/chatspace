e042d6937097ace9c5e2bcd62fa36e7635ebb5bb
miyukimaura <maumiyu0412@gmail.com>
2019-06-23 13:37:51 +0900

    routes　修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,13 @@ Rails.application.routes.draw do
     resources :messages, only: [:index, :create]
   end
 
-  resources :users, only: :show do
-    collection do
-      get 'search'
-    end
-  end
-  root 'users#index'
+#  resources :users, only: :show do
+#    collection do
+#      get 'search'
+#    end
+#  end
+#  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+#end
 
 
